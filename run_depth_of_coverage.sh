bd56321bbdeed1b0d779ff956edcedfdd7e1c78d
Dieter Best <dieterbest@gmail.com>
2023-07-06 14:47:50 -0700

    add check with samtools depth

--- run_depth_of_coverage.sh
+++ run_depth_of_coverage.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-BAM=~/Analysis/varpipe3/data/Output_with_tbdb_reference/ERR552797/ERR552797_sdrcsm.bam
+BAM=~/Analysis/varpipe4/tests//ERR552797/ERR552797_sdrcsm.bam
 REF=~/Analysis/varpipe4/references/NC_000962.3/NC_000962.3.fasta 
+BED=/home/dieterbest/Analysis/varpipe4/intervals/tbdb_intervals.bed
 
-~/Analysis/tools/depth_of_coverage.py --bam $BAM \
-				      --reference $REF \
-				      --intervals tbdb.bed \
-				      --lower_coverage 10 \
-				      --output ERR552797 \
-				      --csv ERR552797_depth_of_coverage.csv
+# ~/Analysis/tools/depth_of_coverage.py --bam $BAM \
+# 				      --reference $REF \
+# 				      --intervals $BED \
+# 				      --lower_coverage 10 \
+# 				      --output ERR552797 \
+# 				      --csv ERR552797_depth_of_coverage.csv
+
+# cross check with samtools
+samtools depth -a -b $BED $BAM > samtools_depth.tsv
+
