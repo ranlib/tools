89d12c7aa70a2b4a5f5c0fac76d576ed3e6fc942
Dieter Best <dieterbest@gmail.com>
2023-07-14 22:10:30 -0700

    vcf_to_pandas_dataframe: can now read vcf and compressed vcf

--- vcf_to_pandas_dataframe.py
+++ vcf_to_pandas_dataframe.py
@@ -8,8 +8,6 @@ import vcf
 import pandas
 import argparse
 from collections import OrderedDict
-import io
-import gzip
 
 def vcf_to_pandas_dataframe(vcf_file: str, sample_name: str) -> pandas.DataFrame:
     """
@@ -19,87 +17,80 @@ def vcf_to_pandas_dataframe(vcf_file: str, sample_name: str) -> pandas.DataFrame
     :param str sample_name: name of sample
     :return: pandas data frame
     """
+    vcf_reader = vcf.Reader(filename=vcf_file)
 
-    # open vcf file
-    #if vcf_file.endswith(".gz"):
-    #    F = io.TextIOWrapper(io.BufferedReader(gzip.open(vcf_file, "rb")))
-    #else:
+    # get annotation terms from header
+    annotation = vcf_reader.infos["ANN"].desc
+    _, terms = re.split(r":[ ]+", annotation)
+    ann_keys = terms.replace("'", "").split(" | ")
 
-    with open(vcf_file, "r") as vcf_file_handle:
-        vcf_reader = vcf.Reader(vcf_file_handle)
+    # loop over records
+    ANNS = []
+    for record in vcf_reader:
+        vcf_item = OrderedDict()
+        vcf_item["Sample ID"] = sample_name
+        vcf_item["CHROM"] = record.CHROM
+        vcf_item["POS"] = record.POS
+        #vcf_item["ID"] = "." if not record.ID else record.ID
+        vcf_item["REF"] = record.REF
+        vcf_item["ALT"] = ','.join([ str(n) for n in record.ALT ])
+        #vcf_item["QUAL"] = "." if not record.QUAL else record.QUAL
+        vcf_item["FILTER"] = "." if not record.FILTER else record.FILTER
+        info = record.INFO
+        item = info["ANN"][0]
+        ann_item = dict(zip(ann_keys, item.split("|")))
+        cDNA = ann_item["cDNA.pos / cDNA.length"].split("/")
+        CDS = ann_item["CDS.pos / CDS.length"].split("/")
+        AA = ann_item["AA.pos / AA.length"].split("/")
 
-        # get annotation terms from header
-        annotation = vcf_reader.infos["ANN"].desc
-        _, terms = re.split(r":[ ]+", annotation)
-        ann_keys = terms.replace("'", "").split(" | ")
+        ann_item["cDNA.pos"], ann_item["cDNA.length"] = [-1,-1]
+        ann_item["CDS.pos"], ann_item["CDS.length"] = [-1,-1]
+        ann_item["AA.pos"], ann_item["AA.length"] = [-1,-1]
 
-        # loop over records
-        ANNS = []
-        for record in vcf_reader:
-            vcf_item = OrderedDict()
-            vcf_item["Sample ID"] = sample_name
-            vcf_item["CHROM"] = record.CHROM
-            vcf_item["POS"] = record.POS
-            #vcf_item["ID"] = "." if not record.ID else record.ID
-            vcf_item["REF"] = record.REF
-            vcf_item["ALT"] = ','.join([ str(n) for n in record.ALT ])
-            #vcf_item["QUAL"] = "." if not record.QUAL else record.QUAL
-            vcf_item["FILTER"] = "." if not record.FILTER else record.FILTER
-            info = record.INFO
-            item = info["ANN"][0]
-            ann_item = dict(zip(ann_keys, item.split("|")))
-            cDNA = ann_item["cDNA.pos / cDNA.length"].split("/")
-            CDS = ann_item["CDS.pos / CDS.length"].split("/")
-            AA = ann_item["AA.pos / AA.length"].split("/")
+        if len(cDNA) == 2:
+            ann_item["cDNA.pos"], ann_item["cDNA.length"] = list( map(int, cDNA) )
+        if len(CDS) == 2:
+            ann_item["CDS.pos"], ann_item["CDS.length"] = list( map(int, CDS))
+        if len(AA) == 2:
+            ann_item["AA.pos"], ann_item["AA.length"] = list( map(int, AA))
 
-            ann_item["cDNA.pos"], ann_item["cDNA.length"] = [-1,-1]
-            ann_item["CDS.pos"], ann_item["CDS.length"] = [-1,-1]
-            ann_item["AA.pos"], ann_item["AA.length"] = [-1,-1]
-            
-            if len(cDNA) == 2:
-                ann_item["cDNA.pos"], ann_item["cDNA.length"] = list( map(int, cDNA) )
-            if len(CDS) == 2:
-                ann_item["CDS.pos"], ann_item["CDS.length"] = list( map(int, CDS))
-            if len(AA) == 2:
-                ann_item["AA.pos"], ann_item["AA.length"] = list( map(int, AA))
+        ann_item["Total Read Depth"] = record.genotype(sample_name)["DP"]
+        ann_item["AD_REF"], ann_item["Variant Read Depth"] = record.genotype(sample_name)["AD"]
+        #ann_item["Percent Alt Allele"] = record.genotype(sample_name)["AF"]
+        ann_item["Percent Alt Allele"] = -1 if ann_item["Total Read Depth"] == 0 else ann_item["Variant Read Depth"] * 100.0 / ann_item["Total Read Depth"]
 
-            ann_item["Total Read Depth"] = record.genotype(sample_name)["DP"]
-            ann_item["AD_REF"], ann_item["Variant Read Depth"] = record.genotype(sample_name)["AD"]
-            #ann_item["Percent Alt Allele"] = record.genotype(sample_name)["AF"]
-            ann_item["Percent Alt Allele"] = -1 if ann_item["Total Read Depth"] == 0 else ann_item["Variant Read Depth"] * 100.0 / ann_item["Total Read Depth"]
+        # kick out some keys
+        forget_this = [ "Allele",
+                        "Feature_Type",
+                        "Feature_ID",
+                        "Transcript_BioType",
+                        "Rank",
+                        "cDNA.pos / cDNA.length",
+                        "CDS.pos / CDS.length",
+                        "AA.pos / AA.length",
+                        "Distance",
+                        "ERRORS / WARNINGS / INFO ",
+                        "cDNA.pos",
+                        "cDNA.length",
+                        "CDS.length",
+                        "AA.pos",
+                        "AA.length",
+                        "AD_REF"
+                       ]
+        for item in forget_this:
+            if item in ann_item:
+                ann_item.pop(item)
 
-            # kick out some keys
-            forget_this = [ "Allele",
-                            "Feature_Type",
-                            "Feature_ID",
-                            "Transcript_BioType",
-                            "Rank",
-                            "cDNA.pos / cDNA.length",
-                            "CDS.pos / CDS.length",
-                            "AA.pos / AA.length",
-                            "Distance",
-                            "ERRORS / WARNINGS / INFO ",
-                            "cDNA.pos",
-                            "cDNA.length",
-                            "CDS.length",
-                            "AA.pos",
-                            "AA.length",
-                            "AD_REF"
-                           ]
-            for item in forget_this:
-                if item in ann_item:
-                    ann_item.pop(item)
+        # rename some keys
+        ann_item['Gene Name'] = ann_item.pop("Gene_Name")
+        ann_item['Gene ID'] = ann_item.pop("Gene_ID")
+        ann_item['Nucleotide Change'] = ann_item.pop("HGVS.c")
+        ann_item['Amino acid Change'] = ann_item.pop("HGVS.p")
+        ann_item['Position within CDS'] = ann_item.pop("CDS.pos")
 
-            # rename some keys
-            ann_item['Gene Name'] = ann_item.pop("Gene_Name")
-            ann_item['Gene ID'] = ann_item.pop("Gene_ID")
-            ann_item['Nucleotide Change'] = ann_item.pop("HGVS.c")
-            ann_item['Amino acid Change'] = ann_item.pop("HGVS.p")
-            ann_item['Position within CDS'] = ann_item.pop("CDS.pos")
-                        
-            ANNS.append(vcf_item | ann_item)
+        ANNS.append(vcf_item | ann_item)
 
-        df = pandas.DataFrame(ANNS)
+    df = pandas.DataFrame(ANNS)
     return df
 
 
