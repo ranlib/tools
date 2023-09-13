392bc5b5aa0796240a58458c9cd58642eab59a3f
Dieter Best <dieterbest@gmail.com>
2023-09-12 22:52:05 -0700

    lims_report: add logging, change term chemical -> drug, in input lab report change severity of mutations with weak evidence to WT, remove bed file as it is not needed, take chemical list and gene list per chemical from hard coded dictionaries, cleaner code

--- lims_report.py
+++ lims_report.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 """
 lab report ==> lims report
+Note:
+1) list of chemicals hardcoed
+2) list of genes considered/chemical hardcoded
 """
 import argparse
 import datetime
@@ -18,53 +21,53 @@ logging.basicConfig(encoding="utf-8")
 RRDR = pandas.Interval(left=426, right=452, closed="both")
 
 
-def get_chemical_evaluation(severity: str, antimicrobial: str, gene: str, annotation: str, amino_acid_change: str, position_within_cds: int) -> str:
+def get_drug_evaluation(severity: str, antimicrobial: str, gene: str, annotation: str, amino_acid_change: str, position_within_cds: int) -> str:
     """
-    get chemical evalution based on severity and other variables
+    get drug evalution based on severity and other variables
     :param str severity
     :param str antimicrobial
     :param str gene
     :param str annotation
     :param str amino_acid_change
-    :return chemical_evaluation
+    :return drug_evaluation
     """
-    chemical_evaluation = ""
+    drug_evaluation = ""
 
-    if severity in ["R", "R-interim"]:
+    if severity == "R":
         if gene == "rpoB":
             if amino_acid_change in ["Leu430Pro", "Asp435Tyr", "His445Asn", "His445Ser", "His445Leu", "His445Cys", "Leu452Pro", "Ile491Phe"]:
-                chemical_evaluation = "Predicted low-level resistance to rifampin; may test susceptible by phenotypic methods"
+                drug_evaluation = "Predicted low-level resistance to rifampin; may test susceptible by phenotypic methods"
             else:
-                chemical_evaluation = "Predicted resistance to rifampin"
+                drug_evaluation = "Predicted resistance to rifampin"
         else:
-            chemical_evaluation = f"Mutation(s) associated with resistance to {antimicrobial} detected"
+            drug_evaluation = f"Mutation(s) associated with resistance to {antimicrobial} detected"
 
     if severity == "U":
-        chemical_evaluation = f"The detected mutation(s) have uncertain significance; resistance to {antimicrobial} cannot be ruled out"
+        drug_evaluation = f"The detected mutation(s) have uncertain significance; resistance to {antimicrobial} cannot be ruled out"
 
-    if severity in ["S", "S-interim"]:
+    if severity == "S":
         if antimicrobial != "rifampin":
-            chemical_evaluation = f"No mutations associated with resistance to {antimicrobial} detected"
+            drug_evaluation = f"No mutations associated with resistance to {antimicrobial} detected"
         else:  # rifampin
             if (annotation == "synonymous_variant") and (position_within_cds in RRDR):
-                chemical_evaluation = "Predicted susceptibility to rifampin. The detected synonymous mutation(s) do not confer resistance"
+                drug_evaluation = "Predicted susceptibility to rifampin. The detected synonymous mutation(s) do not confer resistance"
             else:
-                chemical_evaluation = "Predicted susceptibility to rifampin"
+                drug_evaluation = "Predicted susceptibility to rifampin"
 
     if severity == "WT":
-        chemical_evaluation = f"No mutations associated with resistance to {antimicrobial} detected"
+        drug_evaluation = f"No mutations associated with resistance to {antimicrobial} detected"
 
     if severity == "Insufficient Coverage":
-        chemical_evaluation = "Pending Retest"
+        drug_evaluation = "Pending Retest"
 
-    return chemical_evaluation
+    return drug_evaluation
 
 
-def get_gene_chemical_evaluation(chem_gene: pandas.DataFrame, gene: str) -> str:
+def get_gene_drug_evaluation(chem_gene: pandas.DataFrame, gene: str) -> str:
     """
-    get evaluation of chemical/gene combination
+    get evaluation of drug/gene combination
     """
-    severities = chem_gene["looker"].to_list()
+    severities = chem_gene["mdl"].to_list()
     nt_changes = chem_gene["Nucleotide_Change"].to_list()
     aa_changes = chem_gene["Amino_acid_Change"].to_list()
     aa_changes_with_parens = [f"({x})" for x in aa_changes]
@@ -74,7 +77,7 @@ def get_gene_chemical_evaluation(chem_gene: pandas.DataFrame, gene: str) -> str:
     chem_gene_eval = []
     mut_counter = {"R": 0, "U": 0, "S": 0}
     for i, severity in enumerate(severities):
-        if severity in ["R", "R-interim"]:
+        if severity == "R":
             chem_gene_eval.append(nt_changes[i] + " " + aa_changes_with_parens[i])
             mut_counter["R"] += 1
 
@@ -82,7 +85,7 @@ def get_gene_chemical_evaluation(chem_gene: pandas.DataFrame, gene: str) -> str:
             chem_gene_eval.append(nt_changes[i] + " " + aa_changes_with_parens[i])
             mut_counter["U"] += 1
 
-        if severity in ["S", "S-interim"]:
+        if severity == "S":
             if (gene == "rpoB") and (annotations[i] == "synonymous_variant") and (positions_within_cds[i] in RRDR):
                 chem_gene_eval.append(nt_changes[i] + " " + aa_changes_with_parens[i] + "[synonymous]")
             mut_counter["S"] += 1
@@ -99,7 +102,7 @@ def get_gene_chemical_evaluation(chem_gene: pandas.DataFrame, gene: str) -> str:
     return "; ".join(chem_gene_eval)
 
 
-def lims_report(lab_tsv: str, bed: str, lineage_name: str, operator: str) -> pandas.DataFrame():
+def lims_report(lab_tsv: str, lineage_name: str, operator: str) -> pandas.DataFrame():
     """
     create lims report
     """
@@ -155,8 +158,8 @@ def lims_report(lab_tsv: str, bed: str, lineage_name: str, operator: str) -> pan
         "Operator",
     ]
 
-    # map input colum for chemical to output lims report column
-    chemical_to_column = {
+    # map input colum for drug to output lims report column
+    drug_to_column = {
         "isoniazid": "M_DST_B01_INH",
         "ethionamide": "M_DST_C01_ETO",
         "rifampin": "M_DST_D01_RIF",
@@ -172,8 +175,8 @@ def lims_report(lab_tsv: str, bed: str, lineage_name: str, operator: str) -> pan
         "linezolid": "M_DST_N01_LZD",
     }
 
-    # map input columns for chemical/gene combination to output lims report columns
-    gene_chemical_to_column = {
+    # map input columns for drug/gene combination to output lims report columns
+    gene_drug_to_column = {
         ("katG", "isoniazid"): "M_DST_B02_katG",
         ("fabG1", "isoniazid"): "M_DST_B03_fabG1",
         ("inhA", "isoniazid"): "M_DST_B04_inhA",
@@ -206,24 +209,20 @@ def lims_report(lab_tsv: str, bed: str, lineage_name: str, operator: str) -> pan
         ("rrl", "linezolid"): "M_DST_N02_rrl",
         ("rplC", "linezolid"): "M_DST_N03_rplC",
     }
-
+     
     # input = lab report
-    lab_cols = ["Sample ID", "Position within CDS", "Nucleotide Change", "Amino acid Change", "Annotation", "Gene Name", "antimicrobial", "Warning", "looker"]
+    lab_cols = ["Sample ID", "Position within CDS", "Nucleotide Change", "Amino acid Change", "Annotation", "Gene Name", "antimicrobial", "Warning", "mdl"]
     lab = pandas.read_csv(lab_tsv, sep="\t", usecols=lab_cols)
     lab.columns = lab.columns.str.replace(" ", "_")
 
-    # consider only variants which do not have a failed QC remark in Warning column of input lab report
-    n_before = len(lab.index)
-    # remove NAs
+    # consider only variants which do not have a failed QC remark in Warning column of input lab report, i.e. set their severity on WT
     lab = lab.fillna("")
-    lab = lab[~lab["Warning"].str.contains("Mutation failed QC")]
-    n_after = len(lab.index)
-    logger.info(f"remove variants with failed QC: number of variants before =  {n_before}, after = {n_after}")
-
-    # looker = category, determine sort order according to severity
+    lab.loc[lab["Warning"].str.contains("Mutation failed QC"), "mdl"] = "WT"
+    
+    # mdl = category, determine sort order according to severity
     # sort by severity, keep only most severe
-    lab["looker"] = pandas.Categorical(lab["looker"], ["R", "Insufficient Coverage", "R-interim", "U", "S-interim", "S", "WT"])
-    lab_sorted = lab.sort_values(["antimicrobial", "looker"], ascending=True)
+    lab["mdl"] = pandas.Categorical(lab["mdl"], ["R", "Insufficient Coverage", "U", "S", "WT"])
+    lab_sorted = lab.sort_values(["antimicrobial", "mdl"], ascending=True)
 
     # output = lims report
     lims = pandas.DataFrame(columns=header)
@@ -232,43 +231,33 @@ def lims_report(lab_tsv: str, bed: str, lineage_name: str, operator: str) -> pan
     lims.loc[0, "Analysis date"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
     lims.loc[0, "Operator"] = operator
 
-    # get chemicals and genes of interest from bed file
-    chemicals = pandas.read_csv(bed, sep="\t", header=None, usecols=[4, 5])
-    chemicals.columns = ["gene", "chemicals"]
-    antimicrobials = chemicals.assign(chemicals=chemicals["chemicals"].str.split(",")).explode("chemicals")
-    chemicals = set(antimicrobials["chemicals"].to_list())
-    genes = set(antimicrobials["gene"].to_list())
-
-    # loop over chemicals and fill lims report
-    for chemical in chemicals:  # chemical from bed file
-        if chemical not in chemical_to_column:  # hardcoded, make sure consistent with chemical from bed file
-            logger.warning(f"{chemical} not in list of chemicals, continuing.")
+    # loop over drugs and fill lims report
+    # use only drugs in drug_to_column list, i.e. some drugs in input bed file are not considered
+    for drug in drug_to_column:
+        filter_chem = "antimicrobial==@drug"
+        chem = lab_sorted.query(filter_chem)
+        chem = chem.reset_index()
+        if len(chem.index) == 0:
+            logger.warning(f"no data for drug {drug}")
         else:
-            filter_chem = "antimicrobial==@chemical"
-            chem = lab_sorted.query(filter_chem)
-            chem = chem.reset_index()
-            if len(chem.index) == 0:
-                logger.warning(f"no data for chemical {chemical}")
-            else:
-                logger.debug(chem)
-                # fill chemical header based on mutation with highest severity
-                gene = chem.iloc[0]["Gene_Name"]
-                severity = chem.iloc[0]["looker"]
-                annotation = chem.iloc[0]["Annotation"]
-                amino_acid_change = chem.iloc[0]["Amino_acid_Change"]
-                position_within_cds = chem.iloc[0]["Position_within_CDS"]
-                lims.loc[0, chemical_to_column[chemical]] = get_chemical_evaluation(severity, chemical, gene, annotation, amino_acid_change, position_within_cds)
+            logger.debug("Chemical information")
+            logger.debug(chem)
+            # fill drug header based on mutation with highest severity
+            gene = chem.iloc[0]["Gene_Name"]
+            severity = chem.iloc[0]["mdl"]
+            annotation = chem.iloc[0]["Annotation"]
+            amino_acid_change = chem.iloc[0]["Amino_acid_Change"]
+            position_within_cds = chem.iloc[0]["Position_within_CDS"]
+            lims.loc[0, drug_to_column[drug]] = get_drug_evaluation(severity, drug, gene, annotation, amino_acid_change, position_within_cds)
 
-                for gene in genes:
-                    key = (gene, chemical)
-                    if key not in gene_chemical_to_column:
-                        logger.debug(f"{gene}, {chemical}) not in map")
-                    else:
-                        # get information for gene/chemical information and fill column
-                        filter_gene = "antimicrobial==@chemical & Gene_Name==@gene"
-                        chem_gene = lab_sorted.query(filter_gene)
-                        logger.debug(chem_gene)
-                        lims.loc[0, gene_chemical_to_column[key]] = get_gene_chemical_evaluation(chem_gene, gene)
+            genes_for_this_chemical = [ key[0] for key in list(gene_drug_to_column.keys()) if key[1] == drug ]
+            for gene in genes_for_this_chemical:
+                # get information for gene/drug information and fill column
+                filter_gene = "antimicrobial==@drug & Gene_Name==@gene"
+                chem_gene = lab_sorted.query(filter_gene)
+                logger.debug("Chemical/gene information")
+                logger.debug(chem_gene)
+                lims.loc[0, gene_drug_to_column[(gene, drug)]] = get_gene_drug_evaluation(chem_gene, gene)
 
     return lims
 
@@ -278,7 +267,6 @@ if __name__ == "__main__":
     parser.add_argument("--lab", "-l", type=argparse.FileType("r"), dest="lab", help="input lab tsv report", required=True)
     parser.add_argument("--operator", "-o", type=str, dest="operator", help="input operator name", required=True)
     parser.add_argument("--lineages", "-s", type=argparse.FileType("r"), dest="lineages", help="input lineage tsv file", nargs="?")
-    parser.add_argument("--bed", "-b", type=argparse.FileType("r"), dest="bed", help="input bed file", required=True)
     parser.add_argument("--lims", "-r", type=argparse.FileType("w"), dest="lims", help="output lims tsv report", required=True)
     parser.add_argument("--log_level", "-d", type=str, dest="log_level", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], default="INFO", help="logging level")
     args = parser.parse_args()
@@ -295,5 +283,5 @@ if __name__ == "__main__":
         lineage = "Lineage information not available"
 
     # create lims report
-    df = lims_report(args.lab, args.bed, lineage, args.operator)
+    df = lims_report(args.lab, lineage, args.operator)
     df.to_csv(args.lims, sep="\t", index=False)
