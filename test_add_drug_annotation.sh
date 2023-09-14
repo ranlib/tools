eb66061008c4b1d8f738fdfcc1b93cdeec882fa7
Dieter Best <dieterbest@gmail.com>
2023-09-14 12:22:18 -0700

    test_add_drug_annotation.sh: minor change

--- test_add_drug_annotation.sh
+++ test_add_drug_annotation.sh
@@ -13,7 +13,11 @@ REF=~/Analysis/varpipe4/references/NC_000962.3/NC_000962.3.fasta
 
 # ~/Analysis/tools/merge_bcftools_SnpSift.py --vcf CDC/ERR552797_DR_loci_raw_annotation.vcf --samplename ERR552797 --output ERR552797_cdc_snpsift.tsv
 
-~/Analysis/tools/add_drug_annotation.py --tsv ERR552797_cdc_snpsift.tsv --minimum_coverage 10 --all_genes\
-					--json tbdb.dr.json --bed tbdb.bed --coverage ERR552797_depth_of_coverage.csv \
-					--output ERR552797_cdc_drugs.tsv --report ERR552797_cdc_interpretation.tsv
+#~/Analysis/tools/add_drug_annotation.py --tsv ERR552797_cdc_snpsift.tsv --minimum_coverage 10 --all_genes\
+#					--json tbdb.dr.json --bed tbdb.bed --coverage ERR552797_depth_of_coverage.csv \
+#					--output ERR552797_cdc_drugs.tsv --report ERR552797_cdc_interpretation.tsv
+
+~/Analysis/tools/variant_interpretation.py --tsv ERR552797_cdc_snpsift.tsv --minimum_coverage 10 --all_genes\
+					   --json tbdb.dr.json --bed tbdb.bed --coverage ERR552797_depth_of_coverage.csv \
+					   --output ERR552797_cdc_drugs.tsv --report ERR552797_cdc_interpretation.tsv
 
