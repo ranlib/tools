d1742cef9ca943c5e52b4ac4db18e78c41dca205
Dieter Best <dieterbest@gmail.com>
2023-09-26 17:27:27 -0700

    get_deletions_in_region: construct deletion interval and check for overlap with regions of interest

--- get_deletions_in_region.py
+++ get_deletions_in_region.py
@@ -5,8 +5,9 @@ determine which regions have at least one large deletion
 import argparse
 import pandas
 import vcf
+import json
 
-def get_deletions_in_region(vcf_file: str, bed: str) -> {}:
+def get_deletions_in_region(vcf_file: str, bed: str, verbose: bool = False) -> {}:
     """
     determine which regions have at least one large deletion
     """
@@ -29,15 +30,22 @@ def get_deletions_in_region(vcf_file: str, bed: str) -> {}:
     vcf_reader = vcf.Reader(filename=vcf_file)
     index_list = [ False for i in genes_list ]
     for record in vcf_reader:
-        #print(record)
         for item in record.ALT:
-            if str(item) == "<DEL>":
-                contained_in_region = regions_array.contains(record.POS)
-                if any(contained_in_region):
-                    indices = [ idx for idx, value  in enumerate(contained_in_region) if value ]
-                    #print(f"deletion at {record.POS} in region {indices}")
-                    for index in indices:
-                        index_list[index] = True
+            if "SVTYPE" in record.INFO:
+                is_SV = True
+            else:
+                is_SV = False
+                
+            if is_SV:
+                if record.INFO["SVTYPE"] == "DEL":
+                    deletion = pandas.Interval(record.POS, record.INFO["END"])
+                    if verbose:
+                        print(deletion.length)
+                    contained_in_region = regions_array.overlaps(deletion)
+                    if any(contained_in_region):
+                        indices = [ idx for idx, value  in enumerate(contained_in_region) if value ]
+                        for index in indices:
+                            index_list[index] = True
 
     return dict(zip(genes_list,index_list))
 
@@ -49,6 +57,5 @@ if __name__ == "__main__":
     parser.add_argument("--bed", "-b", type=argparse.FileType("r"), help="bed file with regions of interest", required=True)
     parser.add_argument("--verbose", action="store_true", help="turn on debugging output")
     args = parser.parse_args()
-    has_deletions = get_deletions_in_region(args.vcf.name, args.bed.name)
-    if args.verbose:
-        print(has_deletions)
+    has_deletions = get_deletions_in_region(args.vcf.name, args.bed.name, args.verbose)
+    print(json.dumps(has_deletions,sort_keys=True, indent=4))
