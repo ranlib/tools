52516eadb16d9cc3bb4d2f2eb76daa2fce941786
Dieter Best <dieterbest@gmail.com>
2024-01-29 10:46:56 -0800

    make_jsons.py: some small modifications

--- make_jsons.py
+++ make_jsons.py
@@ -13,6 +13,7 @@ that fastq file name has the structure samplename_XXXXX.fastq.gz
 """
 import sys
 import os
+import sys
 import json
 import re
 import pathlib
@@ -20,10 +21,17 @@ import argparse
 
 parser = argparse.ArgumentParser(prog="make_json", description="make input json files for varpipe for fastq files in input directory")
 parser.add_argument("--dir", "-d", required=True, type=str, help="Input directory with fastq files")
-parser.add_argument("--json_template", "-t", required=True, type=str, help="Output json file")
+parser.add_argument("--json_template", "-t", required=True, type=argparse.FileType("r"), help="Output json file name")
+parser.add_argument("--json_output_dir", "-o", required=True, type=str, help="Output directory for json files")
 parser.add_argument("--verbose", "-v", action="store_true", help="Verbose output")
 args = parser.parse_args()
 
+try:
+    os.makedirs(args.json_output_dir, exist_ok=True)
+    print("Directory '%s' created successfully" % args.json_output_dir)
+except OSError as error:
+    print("Directory '%s' can not be created" % args.json_output_dir)
+    
 files = []
 for filepath in pathlib.Path(args.dir).glob("**/*.fastq.gz"):
     files.append(str(filepath.absolute()))
@@ -31,16 +39,23 @@ for filepath in pathlib.Path(args.dir).glob("**/*.fastq.gz"):
 if args.verbose:
     print(files)
 
-with open(args.json_template, "r", encoding="ascii") as stream:
+with open(args.json_template.name, "r", encoding="ascii") as stream:
     try:
         json_template = json.load(stream)
     except json.JSONDecodeError as exc:
         print(exc)
 
 R1s = [r for r in files if "_R1" in r]
+<<<<<<< Updated upstream
 # print(R1s)
 if len(R1s) == 0:
-    print(f"<E> length of fastq list = {len(R1s)}")
+    print(f"<E> make_jsons: length of fastq list = {len(R1s)}")
+=======
+#R1s = [r for r in files if "_R1_" in r]
+#print(R1s)
+if len(R1s) == 0:
+    print("<E> make_jsons: list of _R1 files has length 0!")
+>>>>>>> Stashed changes
     sys.exit(1)
     
 json_list = []
@@ -50,12 +65,14 @@ for r1 in R1s:
     json_template["wf_varpipe.read1"] = forward_reads
     json_template["wf_varpipe.read2"] = reverse_reads
     json_template["wf_varpipe.samplename"] = re.sub("_.*$", "", os.path.basename(r1))
+    #json_template["wf_varpipe.samplename"] = re.sub("_R1.fastq.gz$", "", os.path.basename(r1))
     json_template["wf_varpipe.outdir"] = json_template["wf_varpipe.samplename"]
+    #json_template["wf_varpipe.report"] = "variant_interpretation.tsv"
     json_list.append(json_template.copy())
 
 for item in json_list:
     samplename = item["wf_varpipe.samplename"]
-    output_json = pathlib.Path(samplename + ".json")
-    # print(output_json)
+    output_json = pathlib.Path(args.json_output_dir, samplename + ".json")
+    #print(output_json)
     with open(output_json, "w", encoding="ascii") as f:
         json.dump(item, f, indent=4)
